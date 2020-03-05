bf4411e57b4c8bad32d6c3247a6943b3484661e9
Sungyong An <soupyong@gmail.com>
2020-03-05 21:15:49 +0900

    Project Initialization

---
+++ app/src/main/java/soup/neumorphism/MainActivity.kt
@@ -0,0 +1,12 @@
+package soup.neumorphism
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
