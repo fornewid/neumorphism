d817ba852280dd2471af686087eb3e5af6881860
Sungyong An <soupyong@gmail.com>
2020-03-09 19:13:52 +0900

    Introduce NeumorphTextView

--- app/src/main/java/soup/neumorphism/MainActivity.kt
+++ app/src/main/java/soup/neumorphism/MainActivity.kt
@@ -1,10 +1,8 @@
 package soup.neumorphism
 
-import android.graphics.BlurMaskFilter
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.doOnPreDraw
 import androidx.core.view.drawToBitmap
@@ -15,17 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<TextView>(R.id.headline_light_shadow).run {
-            doOnPreDraw {
-                blurred()
-            }
-        }
-        findViewById<TextView>(R.id.headline_dark_shadow).run {
-            doOnPreDraw {
-                blurred()
-            }
-        }
 
         findViewById<ImageView>(R.id.card_light_shadow).run {
             doOnPreDraw {
@@ -76,10 +63,5 @@ class MainActivity : AppCompatActivity() {
             }
             .from(drawToBitmap())
             .into(this)
-    }
-
-    private fun TextView.blurred(radius: Float = 5f) {
-        setLayerType(View.LAYER_TYPE_SOFTWARE, null)
-        paint.maskFilter = BlurMaskFilter(radius, BlurMaskFilter.Blur.NORMAL)
     }
 }
---
+++ app/src/main/java/soup/neumorphism/NeumorphTextView.kt
@@ -0,0 +1,111 @@
+package soup.neumorphism
+
+import android.content.Context
+import android.graphics.*
+import android.os.Build
+import android.text.Layout
+import android.text.StaticLayout
+import android.text.TextPaint
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.use
+import androidx.core.view.ViewCompat
+import kotlin.math.max
+
+class NeumorphTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    private val shadowPaint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
+    private var shadowElevation: Float = 5f
+    private var shadowColorLight: Int = ContextCompat.getColor(context, R.color.shadow_light)
+    private var shadowColorDark: Int = ContextCompat.getColor(context, R.color.shadow_dark)
+
+    private var lastTextCache: Bitmap? = null
+    private var lastShadowLight: Bitmap? = null
+    private var lastShadowDark: Bitmap? = null
+
+    init {
+        context.obtainStyledAttributes(attrs, R.styleable.NeumorphTextView).use {
+            shadowElevation = it.getDimension(
+                R.styleable.NeumorphTextView_neumorph_shadowElevation,
+                shadowElevation
+            )
+            shadowColorLight = it.getColor(
+                R.styleable.NeumorphTextView_neumorph_shadowColorLight,
+                shadowColorLight
+            )
+            shadowColorDark =
+                it.getColor(R.styleable.NeumorphTextView_neumorph_shadowColorDark, shadowColorDark)
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if (ViewCompat.isLaidOut(this)) {
+            lastTextCache = buildTextCache().also { origin ->
+                if (lastShadowLight == null) {
+                    lastShadowLight = generateBitmapShadowCache(origin, shadowColorLight)
+                }
+                if (lastShadowDark == null) {
+                    lastShadowDark = generateBitmapShadowCache(origin, shadowColorDark)
+                }
+            }
+            lastShadowLight?.let {
+                canvas.drawBitmap(it, -shadowElevation, -shadowElevation, shadowPaint)
+            }
+            lastShadowDark?.let {
+                canvas.drawBitmap(it, shadowElevation, shadowElevation, shadowPaint)
+            }
+        }
+        super.onDraw(canvas)
+    }
+
+    private fun buildTextCache(): Bitmap {
+        return Bitmap.createBitmap(measuredWidth, measuredHeight, Bitmap.Config.ARGB_8888).apply {
+            val tp = TextPaint(TextPaint.ANTI_ALIAS_FLAG)
+            tp.color = Color.BLACK
+            tp.textSize = textSize
+            tp.typeface = typeface
+            staticLayout(text, tp).draw(Canvas(this))
+        }
+    }
+
+    private fun generateBitmapShadowCache(cache: Bitmap, color: Int, radius: Float = 5f): Bitmap? {
+        val width: Int = measuredWidth
+        val height: Int = measuredHeight
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+
+        // Call mutate, so that the pixel allocation by the underlying vector drawable is cleared.
+        canvas.save()
+        canvas.drawBitmap(cache, 0f, 0f, null)
+        canvas.restore()
+
+        // Draws the shadow from original drawable
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
+        paint.color = color
+        paint.maskFilter = BlurMaskFilter(max(1f, radius), BlurMaskFilter.Blur.NORMAL)
+        val offset = IntArray(2)
+        val shadow = bitmap.extractAlpha(paint, offset)
+        paint.maskFilter = null
+        bitmap.eraseColor(Color.TRANSPARENT)
+        canvas.drawBitmap(shadow, offset[0].toFloat(), offset[1].toFloat(), paint)
+        return bitmap
+    }
+
+    private fun staticLayout(text: CharSequence, textPaint: TextPaint): StaticLayout {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            StaticLayout.Builder
+                .obtain(text, 0, text.length, textPaint, Int.MAX_VALUE)
+                .build()
+        } else {
+            StaticLayout(
+                text, textPaint, Int.MAX_VALUE,
+                Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false
+            )
+        }
+    }
+}
