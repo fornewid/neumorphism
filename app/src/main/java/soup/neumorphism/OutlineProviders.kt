aba4927e70a95bcdbd26d3c562dc2e36cf7afdfb
Sungyong An <soupyong@gmail.com>
2020-03-12 16:06:22 +0900

    Disable databinding

--- app/src/main/java/soup/neumorphism/OutlineProviders.kt
+++ app/src/main/java/soup/neumorphism/OutlineProviders.kt
@@ -3,7 +3,6 @@ package soup.neumorphism
 import android.graphics.Outline
 import android.view.View
 import android.view.ViewOutlineProvider
-import androidx.databinding.BindingAdapter
 
 object OvalOutlineProvider : ViewOutlineProvider() {
 
@@ -30,13 +29,11 @@ class RoundRectOutlineProvider(private val radius: Float) : ViewOutlineProvider(
     }
 }
 
-@BindingAdapter("clipToOval")
 fun View.setClipToOval(doClip: Boolean) {
     clipToOutline = doClip
     outlineProvider = if (doClip) OvalOutlineProvider else null
 }
 
-@BindingAdapter("clipToRoundRect")
 fun View.setClipToRoundRect(clipRadius: Float) {
     clipToOutline = true
     outlineProvider = RoundRectOutlineProvider(clipRadius)
