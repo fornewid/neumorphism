5c7073db1cfb86f08221466f13853ee164355b04
Sungyong An <soupyong@gmail.com>
2020-05-07 18:00:29 +0900

    Library: Introduce NeumorphImageButton

---
+++ neumorphism/src/main/java/soup/neumorphism/NeumorphImageButton.kt
@@ -0,0 +1,99 @@
+package soup.neumorphism
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.util.Log
+import androidx.annotation.ColorInt
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.AppCompatImageView
+
+class NeumorphImageButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = R.attr.neumorphImageButtonStyle,
+    defStyleRes: Int = R.style.Widget_Neumorph_ImageButton
+) : AppCompatImageButton(context, attrs, defStyleAttr) {
+
+    private val shapeDrawable: NeumorphShapeDrawable
+
+    init {
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.NeumorphImageButton, defStyleAttr, defStyleRes
+        )
+        val shapeType = a.getInt(R.styleable.NeumorphImageButton_neumorph_shapeType, ShapeType.DEFAULT)
+        val shadowElevation = a.getDimension(
+            R.styleable.NeumorphImageButton_neumorph_shadowElevation, 0f
+        )
+        val shadowColorLight = a.getColor(
+            R.styleable.NeumorphImageButton_neumorph_shadowColorLight, Color.WHITE
+        )
+        val shadowColorDark = a.getColor(
+            R.styleable.NeumorphImageButton_neumorph_shadowColorDark, Color.BLACK
+        )
+        a.recycle()
+
+        shapeDrawable = NeumorphShapeDrawable(context, attrs, defStyleAttr, defStyleRes).apply {
+            setShapeType(shapeType)
+            setShadowElevation(shadowElevation)
+            setShadowColorLight(shadowColorLight)
+            setShadowColorDark(shadowColorDark)
+        }
+        setBackgroundInternal(shapeDrawable)
+    }
+
+    override fun setBackground(drawable: Drawable?) {
+        setBackgroundDrawable(drawable)
+    }
+
+    override fun setBackgroundDrawable(drawable: Drawable?) {
+        Log.i(LOG_TAG, "Setting a custom background is not supported.");
+    }
+
+    private fun setBackgroundInternal(drawable: Drawable?) {
+        super.setBackgroundDrawable(drawable)
+    }
+
+    fun setShapeAppearanceModel(shapeAppearanceModel: NeumorphShapeAppearanceModel) {
+        shapeDrawable.setShapeAppearanceModel(shapeAppearanceModel)
+    }
+
+    fun getShapeAppearanceModel(): NeumorphShapeAppearanceModel {
+        return shapeDrawable.getShapeAppearanceModel()
+    }
+
+    fun setShapeType(@ShapeType shapeType: Int) {
+        shapeDrawable.setShapeType(shapeType)
+    }
+
+    @ShapeType
+    fun getShapeType(): Int {
+        return shapeDrawable.getShapeType()
+    }
+
+    fun setShadowElevation(shadowElevation: Float) {
+        shapeDrawable.setShadowElevation(shadowElevation)
+    }
+
+    fun getShadowElevation(): Float {
+        return shapeDrawable.getShadowElevation()
+    }
+
+    fun setShadowColorLight(@ColorInt shadowColor: Int) {
+        shapeDrawable.setShadowColorLight(shadowColor)
+    }
+
+    fun setShadowColorDark(@ColorInt shadowColor: Int) {
+        shapeDrawable.setShadowColorDark(shadowColor)
+    }
+
+    override fun setTranslationZ(translationZ: Float) {
+        super.setTranslationZ(translationZ)
+        shapeDrawable.setTranslationZ(translationZ)
+    }
+
+    companion object {
+        private const val LOG_TAG = "NeumorphImageView"
+    }
+}
