52c4ff7ccb93afe8e4ca6dcc2a4c01adfaf90c75
SOUP <soupyong@gmail.com>
2020-06-02 22:51:35 +0900

    Sample: Add activity to test color APIs simply

---
+++ sample/src/main/java/soup/neumorphism/sample/ColorSampleActivity.kt
@@ -0,0 +1,61 @@
+package soup.neumorphism.sample
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import soup.neumorphism.sample.databinding.ActivitySampleColorBinding
+
+class ColorSampleActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySampleColorBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySampleColorBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        onBackgroundColorChanged(Color.RED)
+    }
+
+    private fun onBackgroundColorChanged(backgroundColor: Int) {
+        binding.run {
+            card.setBackgroundColor(backgroundColor)
+            imageView.setBackgroundColor(backgroundColor)
+            imageButton.setBackgroundColor(backgroundColor)
+            button.setBackgroundColor(backgroundColor)
+            fab.setBackgroundColor(backgroundColor)
+        }
+    }
+
+    private fun onBackgroundColorChanged(backgroundColor: ColorStateList?) {
+        binding.run {
+            card.setBackgroundColor(backgroundColor)
+            imageView.setBackgroundColor(backgroundColor)
+            imageButton.setBackgroundColor(backgroundColor)
+            button.setBackgroundColor(backgroundColor)
+            fab.setBackgroundColor(backgroundColor)
+        }
+    }
+
+    private fun onStrokeColorChanged(strokeColor: ColorStateList?) {
+        binding.run {
+            card.setStrokeColor(strokeColor)
+            imageView.setStrokeColor(strokeColor)
+            imageButton.setStrokeColor(strokeColor)
+            button.setStrokeColor(strokeColor)
+            fab.setStrokeColor(strokeColor)
+        }
+    }
+
+    private fun onStrokeWidthChanged(strokeWidth: Float) {
+        binding.run {
+            card.setStrokeWidth(strokeWidth)
+            imageView.setStrokeWidth(strokeWidth)
+            imageButton.setStrokeWidth(strokeWidth)
+            button.setStrokeWidth(strokeWidth)
+            fab.setStrokeWidth(strokeWidth)
+        }
+    }
+}
--- sample/src/main/java/soup/neumorphism/sample/MainActivity.kt
+++ sample/src/main/java/soup/neumorphism/sample/MainActivity.kt
@@ -1,6 +1,8 @@
 package soup.neumorphism.sample
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.button).setOnClickListener {
+            startActivity(Intent(this, ColorSampleActivity::class.java))
+        }
     }
 }
