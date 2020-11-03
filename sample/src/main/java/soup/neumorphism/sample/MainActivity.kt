bd83ca5cb003ab02b023a3eab59ac361a0ac2fcb
SOUP <soupyong@gmail.com>
2020-11-03 20:34:45 +0900

    Minimize and clean up codes

--- sample/src/main/java/soup/neumorphism/sample/MainActivity.kt
+++ sample/src/main/java/soup/neumorphism/sample/MainActivity.kt
@@ -2,26 +2,26 @@ package soup.neumorphism.sample
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import soup.neumorphism.sample.collection.CollectionSampleActivity
+import soup.neumorphism.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<View>(R.id.button).setOnClickListener {
+        binding.button.setOnClickListener {
             startActivity(Intent(this, ColorSampleActivity::class.java))
         }
 
-        findViewById<View>(R.id.button2).setOnClickListener {
-            startActivity(Intent(this, CollectionSampleActivity::class.java))
+        binding.flatCard.setOnClickListener {
+            startActivity(Intent(this, CardSampleActivity::class.java))
         }
 
-        findViewById<View>(R.id.flat_card).setOnClickListener {
-            startActivity(Intent(this, CardSampleActivity::class.java))
+        binding.fab.setOnClickListener {
+            startActivity(Intent(this, ListSampleActivity::class.java))
         }
     }
 }
