cd8d78696a5f25ae71eab8b2e386698a4405c3c0
SOUP <soupyong@gmail.com>
2020-11-05 23:42:05 +0900

    Use api 30 for sample

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -2,9 +2,9 @@ package dependencies
 
 object Versions {
     const val minSdk = 21
-    const val targetSdk = 29
-    const val compileSdk = 29
-    const val buildTools = "29.0.3"
+    const val targetSdk = 30
+    const val compileSdk = 30
+    const val buildTools = "30.0.2"
 }
 
 object Libs {
