b75b575b59a86438d97826d744ffa0c1faa97ad6
SOUP <soupyong@gmail.com>
2020-11-06 22:32:39 +0900

    Add 'sample-benchmark' module

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -21,5 +21,20 @@ object Libs {
         const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.4"
         const val core_ktx = "androidx.core:core-ktx:1.3.2"
         const val recyclerview = "androidx.recyclerview:recyclerview:1.1.0"
+
+        object Benchmark {
+            private const val version = "1.0.0"
+            const val plugin = "androidx.benchmark:benchmark-gradle-plugin:$version"
+            const val junit4 = "androidx.benchmark:benchmark-junit4:$version"
+        }
+
+        object Test {
+            const val runner = "androidx.test:runner:1.2.0"
+            const val junit = "androidx.test.ext:junit:1.1.1"
+        }
+    }
+
+    object Test {
+        const val junit = "junit:junit:4.12"
     }
 }
