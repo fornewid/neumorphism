0a565d5615822538e79a24e81d2b7ea9314ef66d
SOUP <soupyong@gmail.com>
2020-11-09 00:23:25 +0900

    Add benchmark codes for RecyclerView

---
+++ sample-benchmark/src/androidTest/java/soup/neumorphism/sample/benchmark/RecyclerViewBenchmark.kt
@@ -0,0 +1,107 @@
+package soup.neumorphism.sample.benchmark
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.FrameLayout.LayoutParams.MATCH_PARENT
+import androidx.benchmark.junit4.BenchmarkRule
+import androidx.benchmark.junit4.measureRepeated
+import androidx.test.annotation.UiThreadTest
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import soup.neumorphism.sample.ui.ListSampleActivity
+
+/**
+ * RecyclerView benchmark - scrolls a RecyclerView, and measures the time taken to reveal each item
+ *
+ * You can use this general approach to benchmark the performance of RecyclerView. Some things to be
+ * aware of:
+ *
+ * - Benchmark one ItemView type at a time. If you have for example section headers, or other types
+ *      of item variation, it's recommended to use fake adapter data with just a single type of item
+ *      at a time.
+ * - If you want to benchmark TextView performance, use randomized text. Reusing words between items
+ *      (such as in this simple test) will artificially perform better than real world usage, due to
+ *      unrealistic layout cache hit rates.
+ * - You won't see the effects of RecyclerView prefetching, or Async text layout with this simple
+ *      approach. We'll add more complex RecyclerView examples as time goes on.
+ *
+ * This benchmark measures the sum of multiple potentially expensive stages of displaying an item:
+ * - Attaching an ItemView to RecyclerView
+ * - Detaching an ItemView (scrolling out of viewport) from RecyclerView
+ * - onBindViewHolder
+ * - ItemView layout
+ *
+ * It does *not* measure any of the following work:
+ * - onCreateViewHolder
+ * - RenderThread and GPU Rendering work
+ */
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class RecyclerViewBenchmark {
+
+    class LazyComputedList<T>(
+        override val size: Int = Int.MAX_VALUE,
+        private inline val compute: (Int) -> T
+    ) : AbstractList<T>() {
+        override fun get(index: Int): T = compute(index)
+    }
+
+    @get:Rule
+    val benchmarkRule = BenchmarkRule()
+
+    @get:Rule
+    val activityRule = ActivityTestRule(ListSampleActivity::class.java)
+
+    @Before
+    fun setup() {
+        activityRule.runOnUiThread {
+            val activity = activityRule.activity
+
+            // Set the RecyclerView to have a height of 1 pixel.
+            // This ensures that only one item can be displayed at once.
+            activity.recyclerView.layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, 1)
+
+            // Initialize the Adapter with fake data.
+            // (Submit null first so both are synchronous for simplicity)
+            // 1st ViewHolder will be inflated and displayed by the next onActivity callback
+            activity.adapter.submitList(null)
+            activity.adapter.submitList(LazyComputedList { buildRandomParagraph() })
+        }
+    }
+
+    @Test
+    fun buildParagraph() {
+        benchmarkRule.measureRepeated {
+            // measure cost of generating paragraph - this is overhead in the primary scroll()
+            // benchmark, but is a very small fraction of the amount of work there.
+            buildRandomParagraph()
+        }
+    }
+
+    @UiThreadTest
+    @Test
+    fun scroll() {
+        val recyclerView = activityRule.activity.recyclerView
+        assertTrue("RecyclerView expected to have children", recyclerView.childCount > 0)
+        assertEquals("RecyclerView must have height = 1", 1, recyclerView.height)
+
+        // RecyclerView has children, its items are attached, bound, and have gone through layout.
+        // Ready to benchmark!
+        benchmarkRule.measureRepeated {
+            // Scroll RecyclerView by one item
+            // this will synchronously execute: attach / detach(old item) / bind / layout
+            recyclerView.scrollBy(0, recyclerView.getLastChild().height)
+        }
+    }
+
+}
+
+private fun ViewGroup.getLastChild(): View = getChildAt(childCount - 1)
--- sample-ui/src/main/java/soup/neumorphism/sample/ui/ListSampleActivity.kt
+++ sample-ui/src/main/java/soup/neumorphism/sample/ui/ListSampleActivity.kt
@@ -4,41 +4,58 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import soup.neumorphism.sample.ui.databinding.ActivitySampleListBinding
 import soup.neumorphism.sample.ui.databinding.ItemCardBinding
 
 class ListSampleActivity : AppCompatActivity() {
 
+    val adapter = Adapter().apply { submitList((0..50).map { "Android" }) }
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivitySampleListBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
-        binding.recyclerView.apply {
-            setHasFixedSize(true)
-            adapter = Adapter(data = (0..50).map { "Android" })
+        recyclerView = binding.recyclerView
+        binding.recyclerView.adapter = adapter
+    }
+
+    class Adapter : ListAdapter<String, Holder>(StringDiffCallback) {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+            return Holder.create(parent)
+        }
+
+        override fun onBindViewHolder(holder: Holder, position: Int) {
+            holder.bind(getItem(position))
         }
     }
 
-    class Adapter(private val data: List<String>) : RecyclerView.Adapter<CardViewHolder>() {
+    class Holder(private val binding: ItemCardBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
-            return CardViewHolder(
-                ItemCardBinding.inflate(
-                    LayoutInflater.from(parent.context),
-                    parent,
-                    false
-                )
-            )
+        fun bind(string: String) {
+            binding.textview.text = string
         }
 
-        override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
-            holder.binding.textview.text = data[position]
+        companion object {
+            fun create(parent: ViewGroup): Holder {
+                val inflater = LayoutInflater.from(parent.context)
+                return Holder(ItemCardBinding.inflate(inflater, parent, false))
+            }
         }
-
-        override fun getItemCount(): Int = data.size
     }
 
-    class CardViewHolder(val binding: ItemCardBinding) : RecyclerView.ViewHolder(binding.root)
+    private object StringDiffCallback : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+    }
 }
